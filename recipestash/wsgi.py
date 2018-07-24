0f1412a3d859bb2bf45cc79065d6d39e8d0ebc9e
Saffron <saffronshuang@gmail.com>
2018-07-24 11:24:48 +0800

    changed settings and wsgi

--- recipestash/wsgi.py
+++ recipestash/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for recipestash project.
+WSGI config for hellowebapp project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -10,9 +10,7 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "recipestash.settings_production")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "recipestash.settings")
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
